d6b28d06aaef14cd3ff4c748cc8dc2dfb145a21a
Aanya <aanyapokhriyal@gmail.com>
2025-08-13 15:25:57 +0530

    port error

--- app.py
+++ app.py
@@ -36,4 +36,5 @@ def get_advice():
     return jsonify(response.json())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
